285205e5583d90a405a7c0206ee6fc9c64b66278
Nam H Bui <77969945+NamHaiBui@users.noreply.github.com>
2024-02-21 18:14:32 -0500

    Completed Order Page

--- app/order/page.js
+++ app/order/page.js
@@ -13,11 +13,63 @@ const page = () => {
           id="OrdersPage"
           className="mt-4 max-w-[1200px] mx-auto px-2 min-h-[50vh]"
         >
-          <div className="bg-white w-full p-6 min-h-[150px]"></div>
-          <div className="flex items-center text-xl">
-            <CiDeliveryTruck className="text-green-500" size={35} />
-            <span className="pl-4">Orders</span>
-            {order.length<1? <div></div>:null}
+          <div className="bg-white w-full p-6 min-h-[150px]">
+            {" "}
+            <div className="flex items-center text-xl">
+              <CiDeliveryTruck className="text-green-500" size={35} />
+              <span className="pl-4">Orders</span>
+            </div>
+            {order.length < 1 ? (
+              <div className="flex items-center justify-center">
+                {" "}
+                You have no order history
+              </div>
+            ) : null}
+            {orders.map((order) => (
+              <div key={order?.id} className="text-sm pl-[50px]">
+                <div className="border-b py-1">
+                  <div className="pt-2">
+                    <span className="font-bold mr-2">Stripe ID:</span>
+                    {order?.stripe_id}
+                  </div>
+
+                  <div className="pt-2">
+                    <span className="font-bold mr-2">Delivery Address:</span>
+                    {order?.name}, {order?.address}, {order?.meetup_address}
+                  </div>
+                  <div className="py-2">
+                    <span className="font-bold mr-2">Meet Up Time:</span>
+                    {moment(order?.meetup_time).calendar()}
+                  </div>
+                  <div className="pt-2">
+                    <span className="font-bold mr-2">Total:</span>$
+                    {order?.total}
+                  </div>
+
+                  <div className="pt-2">
+                    <span className="font-bold mr-2">Order Created:</span>
+                    {moment(order?.created_at).calendar()}
+                  </div>
+                  <div className="flex items-center gap-4">
+                    {order?.orderItem.map((item) => (
+                      <div key={item.id} className="flex items-center">
+                        <Link
+                          className="py-1 hover:underline text-blue-500 font-bold"
+                          href={`/product/${item.product_id}`}
+                        >
+                          <img
+                            className="rounded"
+                            width="120"
+                            src={item.product.url + "/120"}
+                          />
+                          {item.product.title}
+                        </Link>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            ))}
           </div>
         </div>
       </MainLayout>
